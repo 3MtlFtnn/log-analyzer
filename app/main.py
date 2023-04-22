b2fccd63506d0a329040a10fe6d0775816beded4
MamkaTvoya228 <maxim228maxim.sky@gmail.com>
2023-04-22 19:57:55 +0300

    oop in main

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
-import re
-log = './log1.txt'
-lists = []
-a = str(input("Введите первую циферку (5 нулей): "))
-b = str(input("Введите вторую циферку (так же 5 нуликов): "))
-with open(log, 'r', encoding='utf8') as f:
-    for line in f:
-        if re.search(r'XIMSS|CALLLEG', line):
-            print(line)
-        lists.append(line)
-
-
-with open("ximss-a.txt", "w+", encoding='utf8') as file:
-    for line in lists:
-        if re.search(r'XIMSS-'+a, line):
-            file.write(line)
-with open("ximss-b.txt", "w+", encoding='utf8') as file:
-    for line in lists:
-        if re.search(r'XIMSS-'+b, line):
-            file.write(line)
+class flog:
+    def print_console(self):
+        log = '/Users/a111/Developer/Programming/Parser/log1.txt'
+        lists = []
+        with open(log, 'r', encoding='utf8') as f:
+            for line in f:
+                if re.search(r'XIMSS|CALLLEG', line):
+                    print(line)
+                lists.append(line)
+    def save_logs():
+        log = '/Users/a111/Developer/Programming/Parser/log1.txt'
+        lists = []
+        with open(log, 'r', encoding='utf8') as f:
+            for line in f:
+                lists.append(line)
+        a = int(input("Введите число: "))
+        for i in range(a):
+            numbers = input(f"Введите какой код для {i+1}-го XIMISS нужно сохранить (Например XIMISS-000003): ")
+            with open(f"{i+1}.txt", "w+", encoding='utf8') as file:
+                for line in lists:
+                    if re.search(r'XIMSS-' + numbers, line):
+                        file.write(line)
+flog.save_logs()
